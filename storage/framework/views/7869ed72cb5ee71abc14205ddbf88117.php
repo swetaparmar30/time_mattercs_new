22f9086b4e1e4c78960ec344451625a050f92d5d
swetaparmar30 <sweta@karmaasource.com>
2026-04-21 14:49:53 +0530

    add script

--- storage/framework/views/7869ed72cb5ee71abc14205ddbf88117.php
+++ storage/framework/views/7869ed72cb5ee71abc14205ddbf88117.php
@@ -29,6 +29,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Raleway:wght@500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
     <!-- Your CSS -->
     <link rel="stylesheet" href="<?php echo e(asset('front-assets/src/css/admin-dashboard.css')); ?>">
     <link rel="stylesheet" href="<?php echo e(asset('front-assets/src/css/registration.css')); ?>">
